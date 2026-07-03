5dcbb959b6c73885d34e9a3d980b967eb9f9fab4
Max <bwlcerber@gmail.com>
2026-07-04 01:29:55 +0300

    Remove test script

--- test_store.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c=fs.readFileSync('js/store.js','utf8');
-c = c.replace(/window\.PORTAL_DATA/g, 'require("./data_temp.js")');
-fs.writeFileSync('js/store_temp.js', c);
