d6499d3861bcaacb08d7d05c05641bcbc5b6722a
Max <bwlcerber@gmail.com>
2026-07-04 01:38:50 +0300

    Remove temp script

--- replace_design.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('js/data.js', 'utf8');
-content = content.replace(/'Design'/g, "'Web / Landing Pages'");
-fs.writeFileSync('js/data.js', content, 'utf8');
-console.log('Replaced all occurrences of Design');
